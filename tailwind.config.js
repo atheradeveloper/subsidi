620c744286f3a26d2ab7c3470c5208e22f700ee8
Athera Developer <atheradeveloper@gmail.com>
2026-08-13 09:54:22 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Membaca semua file .html dan .js di seluruh folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
